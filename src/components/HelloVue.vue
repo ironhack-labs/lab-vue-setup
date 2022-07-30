11ab8f6f42411884d247ea5b5ffca06b461e1929
droidsam1 <droidsam@gmail.com>
2022-07-30 14:19:23 +0200

    add iteration 3: button to console log

--- src/components/HelloVue.vue
+++ src/components/HelloVue.vue
@@ -1,12 +1,20 @@
 <template>
-  <h1>Hello Vue 💚</h1>
-  <h2>{{ msg }}</h2>
+  <div>
+    <h1>Hello Vue 💚</h1>
+    <h2>{{ msg }}</h2>
+    <button @click="helloWorld">Write hello to console</button>
+  </div>
 </template>
 
 <script>
 export default {
   props: {
     msg: String,
+  },
+  methods: {
+    helloWorld() {
+      console.log("👋 Hello world 🌍");
+    },
   },
 };
 </script>
