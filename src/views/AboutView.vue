fd9ceb337924300e6125240a8669cae3bf4e4361
droidsam1 <droidsam@gmail.com>
2022-07-30 14:41:02 +0200

    add about information

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,15 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>About page</h1>
+    <div>
+      <span>You can find more information about this lab in </span
+      ><a href="https://github.com/ironhack-labs/lab-vue-setup"
+        >lab-vue-setup</a
+      >
+    </div>
+    <div>
+      <span>You can find more information about me in my </span
+      ><a href="https://github.com/droidsam1">github profile</a>
+    </div>
   </div>
 </template>
