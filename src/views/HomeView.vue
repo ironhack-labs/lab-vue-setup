c1aa58f2fbb973d8190e8c01f7cb26c3da6c775d
droidsam1 <droidsam@gmail.com>
2022-07-30 14:35:17 +0200

    remove image navigator from home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,20 +1,16 @@
 <template>
   <div class="home">
     <HelloVue msg="This is my first Vue.js App" />
-    <ImageNavigador />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloVue from "@/components/HelloVue.vue";
-import ImageNavigador from "@/components/ImageNavigador.vue";
-
 export default {
   name: "HomeView",
   components: {
     HelloVue,
-    ImageNavigador,
   },
 };
 </script>
