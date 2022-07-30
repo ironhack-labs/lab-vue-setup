dbb34770b780983384ebebedbeba89951b2e2b84
droidsam1 <droidsam@gmail.com>
2022-07-30 13:04:27 +0200

    add new component. disable router for now.

---
+++ src/components/HelloVue.vue
@@ -0,0 +1,22 @@
+<template>
+  <h1>Hello Vue 💚</h1>
+  <h2>{{ msg }}</h2>
+</template>
+
+<script>
+export default {
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<style scoped>
+h2::before {
+  content: "🚧";
+}
+
+h2::after {
+  content: "🚧";
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <HelloVue msg="This is my first Vue.js App" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import HelloVue from "@/components/HelloVue.vue";
 
 export default {
   name: "HomeView",
   components: {
-    HelloWorld,
+    HelloVue,
   },
 };
 </script>
