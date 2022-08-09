36444112496bac34bb9db3dee77b942dd2aadb9a
Elena Vida <elenavidaluc3a@gmail.com>
2022-08-09 20:23:43 +0200

    iteration 3 lab 1

--- src/components/MyComponent.vue
+++ src/components/MyComponent.vue
@@ -1,10 +1,18 @@
-<template>Hello {{ name }}</template>
+<template>
+  Hello {{ name }}
+  <button @click="helloWorld">Hello World</button>
+</template>
 
 <script>
 export default {
   name: "MyComponent",
   props: {
     name: String,
+  },
+  methods: {
+    helloWorld() {
+      console.log("Hello World");
+    },
   },
 };
 </script>
