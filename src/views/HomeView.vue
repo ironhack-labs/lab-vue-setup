f9ca3060345b8bfd683340af226e9359f293c22c
Elena Vida <elenavidaluc3a@gmail.com>
2022-08-09 20:31:17 +0200

    iteration 4 lab 1

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,17 +2,20 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
+    <MyComponent name="Elena" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
+import MyComponent from "@/components/MyComponent.vue";
 
 export default {
   name: "HomeView",
   components: {
     HelloWorld,
+    MyComponent,
   },
 };
 </script>
