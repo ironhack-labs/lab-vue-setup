0ca8599ab7bdc4b7143eae5d985c3e1213b76916
Ari Tomas Bussi <abussy@gmail.com>
2022-08-13 17:11:32 +0200

    iterations 3 & 4

--- vue-setup/src/components/ToDoItem.vue
+++ vue-setup/src/components/ToDoItem.vue
@@ -1,4 +1,5 @@
 <template>
+    <button class="log" @click="helloWorld" >Log</button>
     <label for="todo">{{ message }}
         <input type="checkbox" id="todo" />
     </label>
@@ -8,8 +9,12 @@
 import { ref } from 'vue';
 
 const message = ref('My ToDo Item');
+const helloWorld = () => console.log('Hello world');
 </script>
 
 <style>
-
+    .log {
+        display: block;
+        margin: 20px auto;
+    }
 </style>
