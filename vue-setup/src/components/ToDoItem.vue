43e0b16302ac667372245d9b60be65e08ae55102
Ari Tomas Bussi <abussy@gmail.com>
2022-08-13 14:27:59 +0200

    iteration 1

---
+++ vue-setup/src/components/ToDoItem.vue
@@ -0,0 +1,15 @@
+<template>
+    <label for="todo">My Todo Item
+        <input type="checkbox" id="todo" />
+    </label>
+</template>
+
+<script>
+// export default {
+
+// }
+</script>
+
+<style>
+
+</style>
--- vue-setup/src/views/HomeView.vue
+++ vue-setup/src/views/HomeView.vue
@@ -1,18 +1,16 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ToDoItem />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import ToDoItem from '@/components/ToDoItem.vue';
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld,
+    ToDoItem,
   },
 };
 </script>
