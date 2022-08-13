82bbe57740cf9f754e549073fc4fe13ad1efcbb0
Gianluca <gianluca@inovum-solutions.com>
2022-08-13 13:40:39 +0200

    fix structure

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,12 +3,10 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: [
-    'plugin:vue/vue3-essential',
-    '@vue/airbnb',
-  ],
+  extends: ['plugin:vue/vue3-essential', '@vue/airbnb'],
   parserOptions: {
     parser: '@babel/eslint-parser',
+    requireConfigFile: false,
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
