3251abd4a7448bc94f0feffc9fbc9043ae906bbc
Txell <meritxell@gold.barcelona>
2022-08-13 14:10:39 +0200

    component creation

---
+++ first-vue-project/src/components/ToDoItem.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <!-- <input type="checkbox" name="todo-item" id="todo-item"/>
+    <label for="todo-item">My Todo Item</label> -->
+    <h1> To Do Item </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ToDoItem',
+};
+</script>
+
+<style>
+
+</style>
