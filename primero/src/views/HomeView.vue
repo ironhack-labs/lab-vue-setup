4c174d92bbfd166d9bf9608fcd781548f69528d5
andayanda <carmelaespinola@gmail.com>
2022-08-14 13:10:50 +0200

    Todolist

--- primero/src/views/HomeView.vue
+++ primero/src/views/HomeView.vue
@@ -3,6 +3,7 @@
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
     <SaludoComp></SaludoComp>
+    <ToDoItem></ToDoItem>
   </div>
 </template>
 
@@ -10,12 +11,14 @@
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue';
 import SaludoComp from '@/components/SaludoComp.vue';
+import ToDoItem from '@/components/ToDoItem.vue';
 
 export default {
   name: 'HomeView',
   components: {
     HelloWorld,
     SaludoComp,
+    ToDoItem,
   },
 };
 </script>
