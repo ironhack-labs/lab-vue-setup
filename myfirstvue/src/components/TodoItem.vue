8c3863be3ecba01143933bb5a3dbab0be9dae46e
Irina <irinakhr@mail.ru>
2022-08-16 21:20:18 +0200

    done again

---
+++ myfirstvue/src/components/TodoItem.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>es un componente TodoItem</h1>
+    <p>{{ hello }}</p>
+    <button @click="helloworld">hello</button>
+  </div>
+</template>
+<script>
+export default {
+  name: 'TodoItem',
+  data() {
+    return {
+      hello: '',
+    };
+  },
+  methods: {
+    helloworld() {
+      this.hello = 'HelloWorld';
+    },
+  },
+};
+</script>
