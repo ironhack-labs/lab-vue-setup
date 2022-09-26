aa63cdea6150e096aaa1bcc23e0e8a8bb5ba8aed
Pabl0Parra <pabsblue16@gmail.com>
2022-09-26 15:15:41 +0200

    Iteration 2

--- lab-vue-setup/src/components/MyComponent.vue
+++ lab-vue-setup/src/components/MyComponent.vue
@@ -1,7 +1,15 @@
 <template>
-  <h1>I´M THE FIRST VUE COMPONENT</h1>
+  <div>
+    <h1>I´M THE {{ componentNumber }} {{ language }} COMPONENT</h1>
+    <h3 v-text="date"></h3>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref } from "vue";
 
+let componentNumber = ref("FIRST");
+let language = ref("VUEJs");
+let date = ref("26/09/22");
+</script>
 <style></style>
