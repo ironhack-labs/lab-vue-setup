0bb5ad97bef0f579362dce6a41f6ce398a3e94c3
Pabl0Parra <pabsblue16@gmail.com>
2022-09-27 10:46:10 +0200

    added a bit of styles

--- lab-vue-setup/src/views/AboutView.vue
+++ lab-vue-setup/src/views/AboutView.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="about">
     <h1>About View</h1>
+    <img src="../assets/run.jpg" alt="" />
   </div>
 </template>
 
@@ -9,7 +10,14 @@
   .about {
     min-height: 100vh;
     display: flex;
+    flex-direction: row;
     align-items: center;
+    color: green;
+  }
+
+  img {
+    width: 100%;
+    height: 600px;
   }
 }
 </style>
--- lab-vue-setup/src/views/HomeView.vue
+++ lab-vue-setup/src/views/HomeView.vue
@@ -5,3 +5,11 @@
     <h1>Testing routing. This is home</h1>
   </main>
 </template>
+
+<style scoped>
+h1 {
+  padding: 20px;
+  text-align: center;
+  color: red;
+}
+</style>
